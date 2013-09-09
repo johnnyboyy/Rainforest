0dd64db9a62bf4044fe5fde749024a7b5445fc82
John Zdanis <Zdanis.john@gmail.com>
2013-09-09 17:32:33 -0400

    price is now properly formatted

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ActiveRecord::Base
 	validates :description, presence: true
 	validates :price_in_cents, numericality: {only_integer: true}
 
+
+	def format_price
+		price_in_dollars = "$" + price_in_cents.to_s[0..-3] +"."+ price_in_cents.to_s[-2..-1]
+	end
 end
